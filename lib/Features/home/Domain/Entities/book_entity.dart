d9c332a0acb0085c93b4fbb30b9a72b6f3248840
kareem abdeen <kareem.elshreef18@gmail.com>
2024-07-12 05:44:57 +0300

    add home repostory as  layer of abstraction

---
+++ lib/Features/home/Domain/Entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String title;
+  final num price;
+  final String image;
+  final String autherName;
+  final double rating;
+
+  BookEntity({
+    required this.title,
+    required this.price,
+    required this.image,
+    required this.autherName,
+    required this.rating,
+  });
+}
---
+++ lib/Features/home/Domain/Repos/home_repostory.dart
@@ -0,0 +1,6 @@
+import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> featchNewsBooks();
+}
